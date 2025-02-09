c0eb4cc34c289021c1eaf65f21b7790461cca491
Seventury <seventury@MacBook-Pro-3.local>
2025-02-10 00:45:16 +1100

    android offline access fix

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
 const CACHE_NAME = 'pomodoro-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json'
+  '.',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
